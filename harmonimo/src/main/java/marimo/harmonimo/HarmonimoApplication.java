4c9e75ba7882ec73ec159a1a9910294df9a9e07b
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-07-27 14:28:03 +0900

    초기 파일

---
+++ harmonimo/src/main/java/marimo/harmonimo/HarmonimoApplication.java
@@ -0,0 +1,13 @@
+package marimo.harmonimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonimoApplication.class, args);
+	}
+
+}
---
+++ harmonimo/src/test/java/marimo/harmonimo/HarmonimoApplicationTests.java
@@ -0,0 +1,13 @@
+package marimo.harmonimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
