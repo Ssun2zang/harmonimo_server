44f7a5b5307aa89f8df24eefce589a7f1849ee92
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-08-08 19:50:22 +0900

    롤백

--- harmonimo/src/main/java/marimo/harmonimo/controller/ImageController.java
+++ harmonimo/src/main/java/marimo/harmonimo/controller/ImageController.java
@@ -1,13 +1,22 @@
+package marimo.harmonimo.controller;
+
+import marimo.harmonimo.dto.User.UserImgDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.net.URISyntaxException;
 import java.util.UUID;
+import java.nio.file.Paths;
+
 
 @RestController
 @RequestMapping(produces = "application/json;charset=utf8")
 public class ImageController {
 
-    private static final String UPLOAD_DIR = "/home/ubuntu/.ssh/harmonimo_server/harmonimo/build/resources/main/static/uploadImg/";
+    private static final String UPLOAD_DIR = "static/uploadImg/";
 
     @PostMapping("/uploadImg")
     public ResponseEntity<UserImgDTO> uploadImage(@RequestParam("file") MultipartFile file) {
@@ -17,14 +26,8 @@ public class ImageController {
         }
 
         try {
-            // 폴더 존재 여부 확인 및 생성
-            File uploadFolder = new File(UPLOAD_DIR);
-            if (!uploadFolder.exists()) {
-                uploadFolder.mkdirs();
-            }
-
             String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
-            File saveFile = new File(UPLOAD_DIR + fileName);
+            File saveFile = Paths.get(getClass().getClassLoader().getResource(UPLOAD_DIR).toURI()).resolve(fileName).toFile();
             file.transferTo(saveFile);
 
             String imageUrl = "http://ec2-3-39-175-221.ap-northeast-2.compute.amazonaws.com:8080/uploadImg/" + fileName;
@@ -33,6 +36,8 @@ public class ImageController {
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(userImgDTO);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
 }
