f6df6b842bc8d405f3c863adaa1e7bf2b37d101a
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-08-27 10:39:58 +0900

    통신 올리기

--- harmonimo/src/main/java/marimo/harmonimo/AwsS3Config.java
+++ harmonimo/src/main/java/marimo/harmonimo/AwsS3Config.java
@@ -1,37 +1,56 @@
 package marimo.harmonimo;
 
 
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 
-@Configuration
+@Service
+@PropertySource("classpath:application-security.yml")
+@NoArgsConstructor
 public class AwsS3Config {
+    private AmazonS3 s3Client;
+
     @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
 
     @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Bean
-    public AmazonS3 amazonS3Client() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    @PostConstruct
+    public void setS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
 
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-//                .withCredentials(new EnvironmentVariableCredentialsProvider())
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(this.region)
                 .build();
     }
 
+    public String upload(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+
+        s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        return s3Client.getUrl(bucket, fileName).toString();
+    }
 }
